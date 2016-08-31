009fef84e0c393522bc6bb3c66cd7d6779dcff98
Pedro Henrique Kopper <pedrohkopper@gmail.com>
2016-08-31 01:11:56 -0300

    Add first example! (Hello World)

---
+++ exemplos/primeiro-trimestre/01-hello-world.c
@@ -0,0 +1,16 @@
+/**
+ * Exemplo 01 - Hello World!
+ * 
+ * Este é o exemplo mais simples da linguagem C. Consiste apenas em um programa que
+ * imprime a mensagem "Hello World!" e termina sua execução.
+ * Apesar de simples, é um bom programa para se basear na estrutura, além de testar
+ * se o compilador está funcionando corretamente
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("Hello World!");
+	return 0;
+}
